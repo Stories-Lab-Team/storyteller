210057e514b462e19c3304f4de46b5b00fbbd46c
Mis311 <ley.hanamura@gmail.com>
2023-06-14 00:55:20 +0200

    home add basic cards

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+
 export default function Home() {
+  const [cards, setCards] = useState([
+    { id: 1, title: "Card 1" },
+    { id: 2, title: "Card 2" },
+    { id: 3, title: "Card 3" }
+  ]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      HELLO
+    <main className="flex flex-col min-h-screen items-center justify-between p-24 bg-gray-100">
+      <header className="flex justify-center w-full">
+        <img src="your_logo_path_here" alt="Logo" className="h-12"/>
+      </header>
+
+      <section className="flex flex-wrap justify-center">
+        {cards.map(card => (
+          <div key={card.id} className="bg-white shadow-md m-2 p-4 rounded-md">
+            <h2 className="text-lg font-semibold mb-2">{card.title}</h2>
+            <p>Card content goes here...</p>
+          </div>
+        ))}
+      </section>
+
+      <footer className="w-full flex justify-center">
+        <button className="btn btn-primary">Create</button>
+      </footer>
     </main>
   )
 }
