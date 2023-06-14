99b65a9d4dd7e01ee68e10c0c9646a15c6e8c357
Mis311 <ley.hanamura@gmail.com>
2023-06-14 18:53:43 +0200

    theme box

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,64 +1,69 @@
 import { useState } from 'react'
 
 export default function Dashboard() {
-  const [theme, setTheme] = useState('Selected Theme');
-  const [story, setStory] = useState('');
-  const [preview, setPreview] = useState(false);
+  const [theme, setTheme] = useState('Selected Theme')
+  const [story, setStory] = useState('')
+  const [preview, setPreview] = useState(false)
+  const [name, setCreatorname] = useState('name') 
 
   const handleStoryChange = (event) => {
-    setStory(event.target.value);
+    setStory(event.target.value)
   }
 
   const handlePreview = () => {
-    setPreview(!preview);
+    setPreview(!preview)
   }
 
   const handleCreate = async () => {
-    console.log('Creating NFT...');
+    console.log('Creating NFT...')
 
     // connect to the user's wallet
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
 
-    // connect to the smart contract 
-    const contract = new ethers.Contract(contractAddress, contractABI, signer);
+    // connect to the smart contract
+    const contract = new ethers.Contract(contractAddress, contractABI, signer)
 
     try {
-      const transaction = await contract.mint(userAddress);
-      await transaction.wait();
+      const transaction = await contract.mint(userAddress)
+      await transaction.wait()
 
-      console.log('NFT minted successfully!');
+      console.log('NFT minted successfully!')
     } catch (error) {
-      console.error('An error occurred while trying to mint the NFT:', error);
+      console.error('An error occurred while trying to mint the NFT:', error)
     }
-}
-
+  }
 
   return (
     <main className="flex flex-col items-center p-12 bg-gray-100 min-h-screen">
-      <h1 className='text-2xl font-bold mb-6'>Dashboard</h1>
+      <h1 className="text-2xl font-bold mb-6">Dashboard</h1>
 
-      <h2 className='text-xl mb-4'>Theme: {theme}</h2>
+      <div className="card w-full md:w-2/3 mb-6 bg-white p-4">
+        <h2 className="text-xl mb-4">Theme: {theme}</h2>
+        <h3 className="text-lg mb-4">By {name}</h3>  
+      </div>
 
       <textarea
-        className='border rounded p-2 w-full md:w-2/3 h-64 mb-6'
+        className="border rounded p-2 w-full md:w-2/3 h-64 mb-6"
         value={story}
         onChange={handleStoryChange}
-        placeholder='Write your story here...'
+        placeholder="Write your story here..."
       />
 
-      <button className='btn btn-primary mr-2 mb-2' onClick={handlePreview}>
+      <button className="btn btn-primary mr-2 mb-2" onClick={handlePreview}>
         {preview ? 'Hide' : 'Preview'}
       </button>
 
-      {preview &&
-        <div className='bg-white border rounded p-4 w-full md:w-2/3 mb-6'>
-          <h3 className='text-lg font-semibold mb-2'>Story Preview</h3>
+      {preview && (
+        <div className="bg-white border rounded p-4 w-full md:w-2/3 mb-6">
+          <h3 className="text-lg font-semibold mb-2">Story Preview</h3>
           <p>{story}</p>
         </div>
-      }
+      )}
 
-      <button className='btn btn-primary' onClick={handleCreate}>Create NFT</button>
+      <button className="btn btn-primary" onClick={handleCreate}>
+        Create NFT
+      </button>
     </main>
   )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Link from "next/link"
+import Link from 'next/link'
 export default function Home() {
   const [cards, setCards] = useState([
     { id: 1, title: 'Card 1', image: 'https://via.placeholder.com/150' },
@@ -9,34 +9,34 @@ export default function Home() {
 
   return (
     <main className="flex flex-col min-h-screen items-start justify-between p-12 bg-gray-100">
-     
       <section className="flex flex-col justify-start w-full mt-0">
-      <h1 className='text-2xl font-bold mb-0 flex flex-col'>Open Challenges</h1>
-      <div className='flex-row flex'>
-        {cards.map((card) => (
-          <div
-            key={card.id}
-            className="bg-white shadow-md m-4 p-4 rounded-md flex  md:flex-row items-start"
-          >
-            <img
-              src={card.image}
-              className="m-4 rounded-md w-50 h-auto flex-shrink-0"
-            ></img>
-            <div className="flex flex-col items-start ml-4">
-              <h2 className="text-lg font-semibold mb-2">{card.title}</h2>
-              <p className="mb-2">Card content goes here...</p>
-              <span className="text-sm text-gray-500 mb-2">#Tag1 #Tag2</span>
-              <button className="btn btn-primary">Join</button>
+        <h1 className="text-2xl font-bold mb-0 flex flex-col">
+          Open Challenges
+        </h1>
+        <div className="flex-row flex">
+          {cards.map((card) => (
+            <div
+              key={card.id}
+              className="bg-white shadow-md m-4 p-4 rounded-md flex  md:flex-row items-start"
+            >
+              <img
+                src={card.image}
+                className="m-4 rounded-md w-50 h-auto flex-shrink-0"
+              ></img>
+              <div className="flex flex-col items-start ml-4">
+                <h2 className="text-lg font-semibold mb-2">{card.title}</h2>
+                <p className="mb-2">Card content goes here...</p>
+                <span className="text-sm text-gray-500 mb-2">#Tag1 #Tag2</span>
+                <button className="btn btn-primary">Join</button>
+              </div>
             </div>
-          </div>
-          
-        ))}
+          ))}
         </div>
       </section>
 
       <footer className="w-full flex justify-center">
         <Link href="/dashboard">
-        <button className="btn btn-primary" >Create</button>
+          <button className="btn btn-primary">Create</button>
         </Link>
       </footer>
     </main>
