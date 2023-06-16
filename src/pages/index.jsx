aec0b2535e70baaf6c07f66c4b2e943cc9659d1e
Mis311 <ley.hanamura@gmail.com>
2023-06-16 19:06:53 +0200

    images bar on landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 
 export default function Home() {
@@ -124,176 +124,192 @@ export default function Home() {
     { title: 'Forgotten Civilizations', creator: 'UserJ', collabs: 14 },
   ]
 
+  const images = [
+    'starship.png',
+    'myth.png',
+    'japan.png',
+    'drama.png',
+  ];
+
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIndex((current) => (current + 1) % images.length);
+    }, 5000); // changes every 5 secs
+
+    return () => clearInterval(timer); // cleanup on unmount
+  }, []);
   return (
-    <main className="flex flex-col min-h-screen items-start justify-between p-12 bg-gray-950">
+    <>
+      {' '}
+      {/* Hero Section */}
+      <section
+      className="w-full h-80 text-center bg-cover bg-center  text-white flex items-center justify-center"
+      style={{
+        backgroundImage: `url(${images[index]})`,
+        transition: 'background-image 1s ease-in-out',
+        backgroundColor: '#f9fafb', 
+        backgroundBlendMode: 'multiply' // blend mode
+      }}
+    >
+      <h1 className="text-3xl font-bold my-4">Storytellers, Unite!</h1>
+      <p className="text-xl">Fuse your imaginations...</p>
+    </section>
 
-
-           {/* Hero Section */}
-           <section className="w-full mb-12 text-center">
-  <div
-    style={{
-      backgroundImage: "url('starship.png')",
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      height: '300px',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      color: '#ffffff',
-    }}
-    className="rounded-lg"
-  >
-
-          <h1 className="text-3xl font-bold my-4">Storytellers, Unite!</h1>
-          <p className="text-xl">Fuse your imaginations, tell old and new stories, join creative hackathons and expand your possibilities.</p>
-        </div>
-      </section>
-      <section className="flex flex-col justify-start w-full mt-0">
-        <h1 className="text-2xl text-white font-bold mb-0 flex flex-col">
-          Open Challenges
-        </h1>
-        <div className="flex-row flex">
-          {cards.map((card) => (
-            <div
-              key={card.id}
-              className="bg-white shadow-md m-4 p-4 rounded-md flex  md:flex-row items-start space-between w-1/3"
-            >
-              <img
-                src={card.image}
-                className="m-4 rounded-md w-50 h-auto "
-              ></img>
-              <div className="flex flex-col items-start">
-                <h2 className="text-lg font-semibold mb-2">{card.title}</h2>
-                <p
-                  className="mb-2 flex-wrap"
-                  style={{ height: '50px', overflow: 'hidden' }}
-                >
-                  {card.description}
-                </p>
-                <span className="text-sm text-gray-500 mb-2">
-                  #{card.media}
-                </span>
-                <Link href={`/hackathon/${card.id}`}>
-                  <button className="btn btn-primary">Detail ▶</button>
-                </Link>
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
-      <section className="mt-4 mb-4 w-full min-w-screen flex flex-col justify-between items-center w-full mt-0 bg-blue-200 z-10">
-        {' '}
-        <Link href="/dashboard">
-          <button className="m-4 bg-white rounded p-4">
-            Create on theme or bluesky
-          </button>
-        </Link>
-      </section>
-
-      <section className="w-full flex mt-8 mb-8">
-        <div className="w-1/2 pr-2 ">
-          <h2 className="text-xl font-bold text-white mb-4">🔥 Hot Stories </h2>
-          {hotStories.map((story, index) => (
-            <Link href="/content">
+      <main className="flex flex-col min-h-screen items-start justify-between p-12 bg-gray-950">
+        <section className="flex flex-col justify-start w-full mt-0">
+          <h1 className="text-2xl text-white font-bold mb-0 flex flex-col">
+            Open Challenges
+          </h1>
+          <div className="flex-row flex">
+            {cards.map((card) => (
               <div
-                key={index}
-                className="card bg-white mb-4 flex items-start flex flex-row cursor-pointer"
+                key={card.id}
+                className="bg-white shadow-md m-4 p-4 rounded-md flex  md:flex-row items-start space-between w-1/3"
               >
                 <img
-                  src={story.imageUrl}
-                  className="w-20 mr-4"
-                  alt={story.title}
-                />
-                <div className="flex">
-                  <h3 className="font-bold mb-2">{story.title}</h3>
-                  <p className="mb-2">{story.description}</p>
-                  <div className="flex justify-between items-center">
-                    <p className="text-sm text-gray-500">
-                      Collaborations: {story.collabs}
-                    </p>
-                    <p className="text-sm text-red-500">
-                      {story.daysLeft} days left
-                    </p>
-                  </div>
+                  src={card.image}
+                  className="m-4 rounded-md w-50 h-auto "
+                ></img>
+                <div className="flex flex-col items-start">
+                  <h2 className="text-lg font-semibold mb-2">{card.title}</h2>
+                  <p
+                    className="mb-2 flex-wrap"
+                    style={{ height: '50px', overflow: 'hidden' }}
+                  >
+                    {card.description}
+                  </p>
+                  <span className="text-sm text-gray-500 mb-2">
+                    #{card.media}
+                  </span>
+                  <Link href={`/hackathon/${card.id}`}>
+                    <button className="btn btn-primary">Detail ▶</button>
+                  </Link>
                 </div>
               </div>
+            ))}
+          </div>
+        </section>
+        <section className="mt-4 mb-4 w-full min-w-screen flex flex-col justify-between items-center w-full mt-0 bg-blue-200 z-10">
+          {' '}
+          <Link href="/dashboard">
+            <button className="m-4 bg-white rounded p-4">
+              Create on theme or bluesky
+            </button>
+          </Link>
+        </section>
+
+        <section className="w-full flex mt-8 mb-8">
+          <div className="w-1/2 pr-2 ">
+            <h2 className="text-xl font-bold text-white mb-4">
+              🔥 Hot Stories{' '}
+            </h2>
+            {hotStories.map((story, index) => (
+              <Link href="/content">
+                <div
+                  key={index}
+                  className="card bg-white mb-4 flex items-start flex flex-row cursor-pointer"
+                >
+                  <img
+                    src={story.imageUrl}
+                    className="w-20 mr-4"
+                    alt={story.title}
+                  />
+                  <div className="flex">
+                    <h3 className="font-bold mb-2">{story.title}</h3>
+                    <p className="mb-2">{story.description}</p>
+                    <div className="flex justify-between items-center">
+                      <p className="text-sm text-gray-500">
+                        Collaborations: {story.collabs}
+                      </p>
+                      <p className="text-sm text-red-500">
+                        {story.daysLeft} days left
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            ))}
+            <Link href="/stories">
+              <p className="btn btn-primary mt-4">View All</p>
             </Link>
-          ))}
-          <Link href="/stories">
-            <p className="btn btn-primary mt-4">View All</p>
-          </Link>
-        </div>
-
-        <div className="w-1/2 pl-2">
-          <h2 className="text-xl   text-white font-bold mb-4">🆕 New Theme List</h2>
-          <div className="grid grid-cols-3 gap-4 text-sm font-semibold mb-2">
-            <p className="col-span-1">Theme</p>
-            <p className="col-span-1">Collaborations</p>
-            <p className="col-span-1">Creator</p>
           </div>
-          {newThemes.map((theme, index) => (
-            <div
-              key={index}
-              className="text-white grid grid-cols-3 gap-6 mb-2 p-2 cursor-pointer shadow hover:shadow-md"
-            >
-              <p className="col-span-1">{theme.title}</p>
 
-              <p className="col-span-1">{theme.collabs}</p>
-              <p className="col-span-1">{theme.creator}</p>
+          <div className="w-1/2 pl-2">
+            <h2 className="text-xl   text-white font-bold mb-4">
+              🆕 New Theme List
+            </h2>
+            <div className="grid grid-cols-3 gap-4 text-sm font-semibold mb-2">
+              <p className="col-span-1">Theme</p>
+              <p className="col-span-1">Collaborations</p>
+              <p className="col-span-1">Creator</p>
             </div>
-          ))}
-          <Link href="/themes">
-            <p className="btn btn-primary mt-4">View All</p>
-          </Link>
-        </div>
-      </section>
-      <footer className="w-full bg-gray-800 text-white py-8">
-        <div className="container mx-auto px-6 md:px-12 flex flex-wrap justify-between items-center">
-          <div>
-            <h3 className="text-xl  text-white font-bold">Storytellers</h3>
-            <p className="mt-2 text-sm">Creating and sharing unique stories</p>
+            {newThemes.map((theme, index) => (
+              <div
+                key={index}
+                className="text-white grid grid-cols-3 gap-6 mb-2 p-2 cursor-pointer shadow hover:shadow-md"
+              >
+                <p className="col-span-1">{theme.title}</p>
+
+                <p className="col-span-1">{theme.collabs}</p>
+                <p className="col-span-1">{theme.creator}</p>
+              </div>
+            ))}
+            <Link href="/themes">
+              <p className="btn btn-primary mt-4">View All</p>
+            </Link>
           </div>
+        </section>
+        <footer className="w-full bg-gray-800 text-white py-8">
+          <div className="container mx-auto px-6 md:px-12 flex flex-wrap justify-between items-center">
+            <div>
+              <h3 className="text-xl  text-white font-bold">Storytellers</h3>
+              <p className="mt-2 text-sm">
+                Creating and sharing unique stories
+              </p>
+            </div>
 
-          <ul className="flex space-x-6 text-white">
-            <li>
-              <Link href="/about">
-                <p>About</p>
-              </Link>
-            </li>
-            <li>
-              <Link href="/contact">
-                <p>Contact</p>
-              </Link>
-            </li>
-            <li>
-              <Link href="/privacy">
-                <p>Privacy Policy</p>
-              </Link>
-            </li>
-            <li>
-              <Link href="/terms">
-                <p>Terms of Service</p>
-              </Link>
-            </li>
-          </ul>
-
-          <div>
-            <h3 className="text-xl font-bold">Follow us:</h3>
-            <div className="flex space-x-4 mt-2 ">
-              <li href="#" cllissNlime="text-xl">
-                <i className="fab fa-facebook"></i>
+            <ul className="flex space-x-6 text-white">
+              <li>
+                <Link href="/about">
+                  <p>About</p>
+                </Link>
               </li>
-              <div href="#" className="text-xl">
-                <li className="fab fa-twitter"></li>
-              </div>
-              <div href="#" className="text-xl">
-                <li className="fab fa-instagram"></li>
+              <li>
+                <Link href="/contact">
+                  <p>Contact</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/privacy">
+                  <p>Privacy Policy</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/terms">
+                  <p>Terms of Service</p>
+                </Link>
+              </li>
+            </ul>
+
+            <div>
+              <h3 className="text-xl font-bold">Follow us:</h3>
+              <div className="flex space-x-4 mt-2 ">
+                <li href="#" cllissNlime="text-xl">
+                  <i className="fab fa-facebook"></i>
+                </li>
+                <div href="#" className="text-xl">
+                  <li className="fab fa-twitter"></li>
+                </div>
+                <div href="#" className="text-xl">
+                  <li className="fab fa-instagram"></li>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </footer>
-    </main>
+        </footer>
+      </main>
+    </>
   )
 }
