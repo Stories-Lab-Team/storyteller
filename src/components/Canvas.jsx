1b5c2c4757cc48bc9e1c29b2858e0ebd0d44e2ce
Mis311 <ley.hanamura@gmail.com>
2023-06-18 02:44:43 +0200

    canvas

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,72 +1,76 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { fabric } from 'fabric';
 
 function Canvas() {
-    
-  const [canvasSize, setCanvasSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
+  const fileInputRef = useRef();
+  const canvasRef = useRef();
 
   useEffect(() => {
-    let canvas = new fabric.Canvas('canvas', {
-      width: canvasSize.width,
-      height: canvasSize.height,
+    const canvas = new fabric.Canvas(canvasRef.current, {
+      width: window.innerWidth,
+      height: window.innerHeight,
     });
 
-    let frame = new fabric.Rect({
-      left: 100,
-      top: 100,
-      fill: 'white',
-      width: 900,
-      height: 400,
-      stroke: 'black',
-      strokeWidth: 3,
-    });
+    for (let i = 0; i < 3; i++) {
+      let leftPos = 100 + i * 300;
 
-    canvas.add(frame);
-    
-    let text = new fabric.IText('Tap and Type', {
-        left: 100,
-        top: 100
+      let frame = new fabric.Rect({
+        left: leftPos,
+        top: 100,
+        fill: 'white',
+        width: 200,
+        height: 100,
+        stroke: 'black',
+        strokeWidth: 3,
       });
-    canvas.add(text);
 
-    fabric.Image.fromURL('path/to/image.jpg', function(img) {
-      // add image onto canvas
-      canvas.add(img);
-    });
-
-    fabric.Image.fromURL('path/to/image.jpg', function(img) {
-      // add image onto canvas
-      canvas.add(img);
-    });
-
-    let json = canvas.toJSON();
-    canvas.loadFromJSON(json);
-
-    let dataUrl = canvas.toDataURL({
-      format: 'png',
-      quality: 0.8
-    });
-
-    const handleResize = () => {
-      setCanvasSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
+      canvas.add(frame);
+      frame.on('mousedown', (event) => {
+        event.stopPropagation();
       });
-      canvas.setWidth(window.innerWidth);
-      canvas.setHeight(window.innerHeight);
-      canvas.renderAll();
+
+      let text = new fabric.IText('Tap and Type', {
+        left: leftPos,
+        top: 100,
+      });
+
+      canvas.add(text);
+      text.on('mousedown', (event) => {
+        event.stopPropagation();
+      });
+      canvas.bringToFront(text);
+    }
+
+    return () => {
+      canvas.getObjects().forEach((object) => {
+        object.off('mousedown');
+      });
     };
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-                  
   }, []);
 
+  const handleImageUpload = (event) => {
+    let reader = new FileReader();
+
+    reader.onload = function (event) {
+      fabric.Image.fromURL(event.target.result, function (img) {
+        img.set({
+          left: 100,
+          top: 100,
+          selectable: false,
+        });
+        canvasRef.current.add(img);
+        canvasRef.current.renderAll();
+      });
+    };
+
+    reader.readAsDataURL(event.target.files[0]);
+  };
+
   return (
-    <canvas id="canvas" width={canvasSize.width} height={canvasSize.height} />
+    <div>
+      <input type="file" ref={fileInputRef} onChange={handleImageUpload} />
+      <canvas ref={canvasRef} />
+    </div>
   );
 }
 
